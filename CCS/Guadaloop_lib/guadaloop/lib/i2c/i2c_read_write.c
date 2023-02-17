44c48bbca673e37e7597026976df8f4b474c3b74
dic425 <danielacaballero@utexas.edu>
2023-02-17 02:28:08 -0600

    i2c_lib
    pull request changes
    
     -added macros
    
     -added documentation using javadocs notation
    
     -cleaned up whitespace
    
    other last minute changes
    
     -modified Transaction_t struct in header file

--- CCS/Guadaloop_lib/guadaloop/lib/i2c/i2c_read_write.c
+++ CCS/Guadaloop_lib/guadaloop/lib/i2c/i2c_read_write.c
@@ -3,191 +3,142 @@
  *
  * Authors: Daniela Caballero, Ty Brinker
  *
+ * @file: functions and data structures to help with i2c communication
+ *
  * */
 #include <stdint.h>
 #include "msp432e401y.h"
 #include <guadaloop/lib/i2c/i2c_read_write.h>
-/*
- *
- * @brief: functions and data structures to help with i2c communication
- *
- */
 
+/** MACROS **/
+#define I2C_INIT_MASTER 0x00000010
+#define PORTB_RCGCGPIO 0x02
+#define PORTG_RCGCGPIO 0x040
+#define PORTL_RCGCGPIO 0x400
+#define PORTK_RCGCGPIO 0x200
+#define PORTA_RCGCGPIO 0x001
+#define I2C_AFSEL_PCTL 0x22
 
 void I2C_init(I2C_Settings_t *i2cSettings){
 
-    //turn on clock for I2C
-    SYSCTL->RCGCI2C |= (1 << (i2cSettings->i2cModule)) ;
+    SYSCTL->RCGCI2C |= (1 << (i2cSettings->i2cModule));
 
-    //turn on clock for GPIO RCGCGPIO
-    //enable alternative function on ports
-    //enable open drain on port
-    //select which alt signal is used on each pin (I2C)
     switch(i2cSettings->i2cModule){
-    case I2CModule_0 | I2CModule_5: //Port B
-        SYSCTL->RCGCGPIO |= 0x02;
-        GPIOB->AFSEL = 1;
-        GPIOB->ODR = 1;
-        switch(i2cSettings->i2cModule){
-        case I2CModule_0:
-            GPIOB->PCTL |= 0x2200;
-            break;
-        case I2CModule_5:
-            GPIOB->PCTL |= 0x22;
-            break;
-        }
-        break;
-    case I2CModule_1:
-        SYSCTL->RCGCGPIO |= 0x040; //Port G
-        GPIOG->AFSEL = 1;
-        GPIOG->ODR = 1;
-        GPIOG->PCTL |= 0x22;
-        break;
-    case I2CModule_2:
-        SYSCTL->RCGCGPIO |= 0x400; //Port L
-        GPIOL->AFSEL = 1;
-        GPIOL->ODR = 1;
-        GPIOL->PCTL |= 0x22;
-        break;
-    case I2CModule_3|I2CModule_4: //Port K
-        SYSCTL->RCGCGPIO |= 0x200;
-        GPIOK->AFSEL = 1;
-        GPIOK->ODR = 1;
-       // GPIOK->PCTL = 0x02;
-        switch(i2cSettings->i2cModule){
-            case I2CModule_3:
-                GPIOK->PCTL |= 0x220000;
+        case I2CModule_0 | I2CModule_5: //Port B
+            SYSCTL->RCGCGPIO |= PORTB_RCGCGPIO;
+            GPIOB->AFSEL = 1;
+            GPIOB->ODR = 1;
+            switch(i2cSettings->i2cModule){
+            case I2CModule_0:
+                GPIOB->PCTL |= I2C_AFSEL_PCTL << 2;
                 break;
-            case I2CModule_4:
-                GPIOK->PCTL |= 0x22000000;
+            case I2CModule_5:
+                GPIOB->PCTL |= I2C_AFSEL_PCTL;
                 break;
-        }
-        break;
-    case I2CModule_6 | I2CModule_7 | I2CModule_8| I2CModule_9: //Port A
-        SYSCTL->RCGCGPIO |= 0x001;
-        GPIOA->AFSEL = 1;
-        GPIOA->ODR = 1;
-        GPIOA->PCTL = 0x02;
-        switch(i2cSettings->i2cModule){
-        case I2CModule_6:
-            GPIOA->PCTL |= 0x22000000;
+            }
             break;
-        case I2CModule_7:
-            GPIOA->PCTL |= 0x220000;
+        case I2CModule_1:
+            SYSCTL->RCGCGPIO |= PORTG_RCGCGPIO; //Port G
+            GPIOG->AFSEL = 1;
+            GPIOG->ODR = 1;
+            GPIOG->PCTL |= I2C_AFSEL_PCTL;
             break;
-        case I2CModule_8:
-            GPIOA->PCTL |= 0x2200;
+        case I2CModule_2:
+            SYSCTL->RCGCGPIO |= PORTL_RCGCGPIO; //Port L
+            GPIOL->AFSEL = 1;
+            GPIOL->ODR = 1;
+            GPIOL->PCTL |= I2C_AFSEL_PCTL;
             break;
-        case I2CModule_9:
-            GPIOA->PCTL |= 0x22;
+        case I2CModule_3|I2CModule_4: //Port K
+            SYSCTL->RCGCGPIO |= PORTK_RCGCGPIO;
+            GPIOK->AFSEL = 1;
+            GPIOK->ODR = 1;
+            switch(i2cSettings->i2cModule){
+                case I2CModule_3:
+                    GPIOK->PCTL |= I2C_AFSEL_PCTL << 4;
+                    break;
+                case I2CModule_4:
+                    GPIOK->PCTL |= I2C_AFSEL_PCTL << 6;
+                    break;
+            }
             break;
-        }
-        break;
-
+        case I2CModule_6 | I2CModule_7 | I2CModule_8| I2CModule_9: //Port A
+            SYSCTL->RCGCGPIO |= PORTA_RCGCGPIO;
+            GPIOA->AFSEL = 1;
+            GPIOA->ODR = 1;
+            switch(i2cSettings->i2cModule){
+            case I2CModule_6:
+                GPIOA->PCTL |= I2C_AFSEL_PCTL << 6;
+                break;
+            case I2CModule_7:
+                GPIOA->PCTL |= I2C_AFSEL_PCTL << 4;
+                break;
+            case I2CModule_8:
+                GPIOA->PCTL |= I2C_AFSEL_PCTL << 2;
+                break;
+            case I2CModule_9:
+                GPIOA->PCTL |= I2C_AFSEL_PCTL;
+                break;
+            }
+            break;
     }
 
-    //initialize I2C master
-    //set clock speed
-    i2cSettings->i2cPort->MCR =  0x00000010;
-    //i2cSettings->i2cPort->MTPR = i2cSettings->bitRate;
-    i2cSettings->i2cPort->MTPR |= 0x09;
-
+    i2cSettings->i2cPort->MCR =  I2C_INIT_MASTER;
+    i2cSettings->i2cPort->MTPR = i2cSettings->bitRate;
 }
 
 
 uint8_t I2C_read_register(Transaction_t *i2cTransaction, I2C_Settings_t *i2cSettings, uint8_t* readBuffer){
-    /*
-     * To read a register via I2C
-     * 1. Write slave addr to I2CMSA
-     * 2. Write register addr to I2CMDR
-     * 3. Switch to RX mode
-     * 4. Read data from I2CMDR
-     *
-     * */
 
-    //write register address first
     i2cSettings->i2cPort->MSA = i2cTransaction->slaveAddress << 1;
     i2cSettings->i2cPort->MSA &= ~I2C_MSA_SA_S;
     i2cSettings->i2cPort->MDR = i2cTransaction->regAddress;
 
     i2cSettings->i2cPort->MCS = I2C_MCS_START|I2C_MCS_RUN;
-    //i2cSettings->i2cPort->MCS &= 0xEF; //xxx0x111
 
     while((i2cSettings->i2cPort->MCS) & I2C_MCS_BUSY != 0){}; //busy bit
     while((i2cSettings->i2cPort->MCS & I2C_MCS_ERROR) >> 1 != 0){ //error bit
-
              if(i2cSettings->i2cPort->MCS & (I2C_MCS_DATACK | I2C_MCS_ADRACK | I2C_MCS_ERROR )){
-
                  i2cSettings->i2cPort->MCS = I2C_MCS_STOP;
                  return 0;
-
              }
-
      }
 
-
-    //switch to RX
     i2cSettings->i2cPort->MSA |= I2C_MSA_SA_S;
-
-   // i2cSettings->i2cPort->MCS &= 0xE7; //xxx01011
     i2cSettings->i2cPort->MCS = I2C_MCS_START|I2C_MCS_RUN;
     uint8_t i;
-    for(i=0; i< i2cTransaction->readCount; i++){
-
+    for(i=0; i< i2cTransaction->byteCount; i++){
         while((i2cSettings->i2cPort->MCS & I2C_MCS_BUSY) !=0 ){};
         while((i2cSettings->i2cPort->MCS & I2C_MCS_ERROR) >> 1 !=0){
-
-        if(i2cSettings->i2cPort->MCS & (I2C_MCS_DATACK | I2C_MCS_ADRACK | I2C_MCS_ERROR )){
-
+            if(i2cSettings->i2cPort->MCS & (I2C_MCS_DATACK | I2C_MCS_ADRACK | I2C_MCS_ERROR )){
                     i2cSettings->i2cPort->MCS = I2C_MCS_STOP;
                     return 0;
-
             }
-
         }
         readBuffer[i] = i2cSettings->i2cPort->MDR;
-        // i2cSettings->i2cPort->MCS &= 0xE9; // xxx01001
         i2cSettings->i2cPort->MCS = I2C_MCS_RUN;
     }
 
-    //i2cSettings->i2cPort->MCS &= 0xE5; //xxx00101
     i2cSettings->i2cPort->MCS = I2C_MCS_STOP;
-
-
     return 1;
-
 }
 
 uint8_t I2C_write_register(Transaction_t *i2cTransaction, I2C_Settings_t *i2cSettings, uint8_t *writeBuffer){
-    /*
-     * To write a register via I2C
-     * 1. Write slave addr to I2CMSA
-     * 2. Write register addr to I2CMDR
-     * 3. Write data to I2CMDR
-     *
-     * */
-
      i2cSettings->i2cPort->MSA |= i2cTransaction->slaveAddress << 1;
      i2cSettings->i2cPort->MSA &= ~I2C_MSA_SA_S;
-     //write data to I2CMDR
      i2cSettings->i2cPort->MDR = i2cTransaction->regAddress;
-    // i2cSettings->i2cPort->MCS |= 0xEF;
      i2cSettings->i2cPort->MCS = I2C_MCS_START|I2C_MCS_RUN;
 
      while((i2cSettings->i2cPort->MCS) & I2C_MCS_BUSY != 0){}; //busy bit
      while((i2cSettings->i2cPort->MCS & I2C_MCS_ERROR) >> 1 != 0){ //error bit
-
          if(i2cSettings->i2cPort->MCS & (I2C_MCS_DATACK | I2C_MCS_ADRACK | I2C_MCS_ERROR )){
-
              i2cSettings->i2cPort->MCS = I2C_MCS_STOP;
              return 0;
-
          }
-
      }
 
      uint8_t i;
-     for(i=0; i<i2cTransaction->writeCount; i++){
+     for(i=0; i<i2cTransaction->byteCount; i++){
          i2cSettings->i2cPort->MDR = writeBuffer[i];
          while((i2cSettings->i2cPort->MCS) & I2C_MCS_BUSY != 0){};
          if(i2cSettings->i2cPort->MCS & (I2C_MCS_DATACK | I2C_MCS_ADRACK | I2C_MCS_ERROR)){
@@ -197,50 +148,39 @@ uint8_t I2C_write_register(Transaction_t *i2cTransaction, I2C_Settings_t *i2cSet
          i2cSettings->i2cPort->MCS = I2C_MCS_RUN;
      }
      i2cSettings->i2cPort->MCS = I2C_MCS_STOP;
-
      return 1;
 }
 
 
 void init_Transaction(Transaction_t *i2cTransaction){
-
-    i2cTransaction->writeCount = 0;
-    i2cTransaction->readCount = 0;
-
+    i2cTransaction->byteCount = 0;
 }
 
 void init_Settings(I2C_Settings_t *i2cSettings){
-
     switch(i2cSettings->i2cModule){
-
-    case I2CModule_0:
-           i2cSettings->i2cPort = I2C0;
-           break;
-    case I2CModule_1:
-           i2cSettings->i2cPort = I2C1;
-           break;
-    case I2CModule_2:
-           i2cSettings->i2cPort = I2C2;
-           break;
-    case I2CModule_3:
-           i2cSettings->i2cPort = I2C3;
-           break;
-    case I2CModule_4:
-           i2cSettings->i2cPort = I2C4;
-           break;
-    case I2CModule_5:
-           i2cSettings->i2cPort = I2C5;
-           break;
-    case I2CModule_6:
-           i2cSettings->i2cPort = I2C6;
-           break;
-    case I2CModule_7:
-           i2cSettings->i2cPort = I2C7;
-           break;
-
+        case I2CModule_0:
+               i2cSettings->i2cPort = I2C0;
+               break;
+        case I2CModule_1:
+               i2cSettings->i2cPort = I2C1;
+               break;
+        case I2CModule_2:
+               i2cSettings->i2cPort = I2C2;
+               break;
+        case I2CModule_3:
+               i2cSettings->i2cPort = I2C3;
+               break;
+        case I2CModule_4:
+               i2cSettings->i2cPort = I2C4;
+               break;
+        case I2CModule_5:
+               i2cSettings->i2cPort = I2C5;
+               break;
+        case I2CModule_6:
+               i2cSettings->i2cPort = I2C6;
+               break;
+        case I2CModule_7:
+               i2cSettings->i2cPort = I2C7;
+               break;
     }
-
 }
-
-
-
--- CCS/Guadaloop_lib/guadaloop/lib/i2c/i2c_read_write.h
+++ CCS/Guadaloop_lib/guadaloop/lib/i2c/i2c_read_write.h
@@ -1,19 +1,34 @@
-/*
+/**
  * i2c_read_write.h
  *
  * Authors: Daniela Caballero
  *
- * */
+ * @file: functions and data structures to help with i2c communication
+ *
+ * Visit MSP432E4 SimpleLink Microcontrollers Technical
+ * Reference Manual page 1313 for more information on i2c
+ *
+ * How to use:
+ * .. create instance of I2C_Settings_t specifying I2C_Modules_t and I2C_Speed_t types
+ *
+ * .. call init_Settings function
+ *
+ * .. create instance of Transaction_t specifying byteCount,
+ * .. slaveAddress, and regAddress
+ *
+ * .. you will change byteCount and regAddress in same instance of Transaction_t
+ * .. when you want read/write a different amount of bytes or read/write to a different
+ * .. register in the sensor
+ *
+ * .. call I2C_read_register or I2C_write_register to begin communication
+ */
 
 #ifndef I2C_READ_WRITE_H_
 #define I2C_READ_WRITE_H_
-/*
- * @brief: functions and data structures to help with i2c communication
- *
- *
+
+/**
+ * enumeration of I2C modules on MSP432
  */
-
-
 typedef enum{
     I2CModule_0 = 0,
     I2CModule_1 = 1,
@@ -28,6 +43,9 @@ typedef enum{
 
 }I2C_Modules_t;
 
+/**
+ * enumeration of I2C clock rate
+ */
 typedef enum{
     standardMode,
     fastMode,
@@ -35,6 +53,11 @@ typedef enum{
     highSpeedMode,
 }I2C_Speed_t;
 
+/**
+ * struct of I2C settings options
+ * @see I2C_Modules_t
+ * @see I2C_Speed_t
+ */
 typedef struct{
    I2C0_Type *i2cPort;
    I2C_Modules_t i2cModule;
@@ -42,34 +65,66 @@ typedef struct{
 
 }I2C_Settings_t;
 
+/**
+ * struct of I2C transaction information
+ */
 typedef struct{
-    uint8_t writeCount;
-    uint8_t readCount;
-    uint8_t slaveAddress;
-    uint8_t regAddress;
+    uint8_t byteCount; /**< how many bytes to read/write from/to sensor */
+    uint8_t slaveAddress; /**< sensor address */
+    uint8_t regAddress; /**< sensor register address */
 
 }Transaction_t;
 
-/*
- * I2C_init function initializes I2C port
- *
- * create I2C_Settings_t instance and set i2cModule
- * and bitRate values then pass as params
- *
- * */
+/**
+ * initializes I2C port
+ * @param i2cSettings as I2C_Settings_t type
+ * @see I2C_Settings_t
+ */
 void I2C_init(I2C_Settings_t *i2cSettings);
-/*
- * I2C_read_register and I2C_write_register send/receive messages
- * to/from sensor
+
+/**
+ * read from sensor register via I2C
+ * 1. write slave addr to I2CMSA
+ * 2. write register addr to I2CMDR
+ * 3. switch to RX mode
+ * 4. read data from I2CMDR
  *
- * sent/received, how many bytes will be sent/received, slave address, and register address
- * pass as params
- *
- * returns 1 if successful, 0 if error
- * */
+ * @param i2cTransaction a pointer to instance of Transaction_t
+ * @param i2cSettings a pointer to instance of I2C_Settings_t
+ * @param readBuffer a pointer to byte array
+ * @see I2C_Settings_t
+ * @see Transaction_t
+ * @return 1 if success 0 if fail
+ */
 uint8_t I2C_read_register(Transaction_t *i2cTransaction, I2C_Settings_t *i2cSettings, uint8_t *readBuffer);
+
+/**
+ * write sensor register via I2C
+ * 1. Write slave addr to I2CMSA
+ * 2. Write register addr to I2CMDR
+ * 3. Write data to I2CMDR
+ *
+ * @param i2cTransaction a pointer to instance of Transaction_t
+ * @param i2cSettings a pointer to instance of I2C_Settings_t
+ * @param writeBuffer a pointer to byte array
+ * @see I2C_Settings_t
+ * @see Transaction_t
+ * @return 1 if success 0 if fail
+ */
 uint8_t I2C_write_register(Transaction_t *i2cTransaction, I2C_Settings_t *i2cSettings, uint8_t *writeBuffer);
+
+/**
+ * initializes Transaction_t instance
+ * @param i2cTransaction a pointer to instance of Transaction_t
+ * @see Transaction_t
+ */
 void init_Transaction(Transaction_t *i2cTransaction);
+
+/**
+ * initializes I2C_Settings_t instance
+ * @param i2cSettings a pointer to instance of I2C_Settings_t
+ * @see I2C_Settings_t
+ */
 void init_Settings(I2C_Settings_t *i2cSettings);
 
 #endif /* GUADALOOP_LIB_I2C_I2C_READ_WRITE_H */
