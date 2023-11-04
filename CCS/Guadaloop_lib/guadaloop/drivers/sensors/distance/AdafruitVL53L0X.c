1866cf83ae5feb86e46058debaa6e2fca630ab75
dic425 <danielacaballero@utexas.edu>
2023-11-04 11:03:48 -0500

    Changes to PR - moved #define's to .c file - made functions static - removed initCommunication() function - changes for readability

--- CCS/Guadaloop_lib/guadaloop/drivers/sensors/distance/AdafruitVL53L0X.c
+++ CCS/Guadaloop_lib/guadaloop/drivers/sensors/distance/AdafruitVL53L0X.c
@@ -2,7 +2,7 @@
  * AdafruitVL53L0X.c
  *
  *  Created on: Oct 11, 2023
- *      Author: Daniela
+ *
  */
 
 
@@ -13,15 +13,78 @@
 #define ONE_BYTE 1
 #define TWO_BYTES 2
 
+
+//sensor address
+#define VL6180X_ADDRESS 0x29
+
+//sensor registers
+#define IDENTIFICATION__MODEL_ID 0x000
+#define IDENTIFICATION__MODEL_REV_MAJOR 0x001
+#define IDENTIFICATION__MODEL_REV_MINOR 0x002
+#define IDENTIFICATION__MODULE_REV_MAJOR 0x003
+#define IDENTIFICATION__MODULE_REV_MINOR 0x004
+#define IDENTIFICATION__DATE_HI 0x006
+#define IDENTIFICATION__DATE_LO 0x007
+#define IDENTIFICATION__TIME_MSB 0x008
+#define IDENTIFICATION__TIME_LSB 0x009
+#define SYSTEM__MODE_GPIO0 0x010
+#define SYSTEM__MODE_GPIO1 0x011
+#define SYSTEM__HISTORY_CTRL 0x012
+#define SYSTEM__INTERRUPT_CONFIG_GPIO 0x014
+#define SYSTEM__INTERRUPT_CLEAR 0x015
+#define SYSTEM__FRESH_OUT_OF_RESET 0x016
+#define SYSTEM__GROUPED_PARAMETER_HOLD 0x017
+#define SYSRANGE__START 0x018
+#define SYSRANGE__THRESH_HIGH 0x019
+#define SYSRANGE__THRESH_LOW 0x01A
+#define SYSRANGE__INTERMEASUREMENT_PERIOD 0x01B
+#define SYSRANGE__MAX_CONVERGENCE_TIME 0x01C
+#define SYSRANGE__CROSSTALK_COMPENSATION_RATE 0x01E
+#define SYSRANGE__CROSSTALK_VALID_HEIGHT 0x021
+#define SYSRANGE__EARLY_CONVERGENCE_ESTIMATE 0x022
+#define SYSRANGE__PART_TO_PART_RANGE_OFFSET 0x024
+#define SYSRANGE__RANGE_IGNORE_VALID_HEIGHT 0x025
+#define SYSRANGE__RANGE_IGNORE_THRESHOLD 0x026
+#define SYSRANGE__MAX_AMBIENT_LEVEL_MULT 0x02C
+#define SYSRANGE__RANGE_CHECK_ENABLES 0x02D
+#define SYSRANGE__VHV_RECALIBRATE 0x02E
+#define SYSRANGE__VHV_REPEAT_RATE 0x031
+#define SYSALS__START 0x038
+#define SYSALS__THRESH_HIGH 0x03A
+#define SYSALS__THRESH_LOW 0x03C
+#define SYSALS__INTERMEASUREMENT_PERIOD 0x03E
+#define SYSALS__ANALOGUE_GAIN 0x03F
+#define SYSALS__INTEGRATION_PERIOD 0x040
+#define RESULT__RANGE_STATUS 0x04D
+#define RESULT__ALS_STATUS 0x04E
+#define RESULT__INTERRUPT_STATUS_GPIO 0x04F
+#define RESULT__ALS_VAL 0x050
+#define RESULT__HISTORY_BUFFER_x
+#define RESULT__RANGE_VAL 0x062
+#define RESULT__RANGE_RAW 0x064
+#define RESULT__RANGE_RETURN_RATE 0x066
+#define RESULT__RANGE_REFERENCE_RATE 0x068
+#define RESULT__RANGE_RETURN_SIGNAL_COUNT 0x06C
+#define RESULT__RANGE_REFERENCE_SIGNAL_COUNT 0x070
+#define RESULT__RANGE_RETURN_AMB_COUNT 0x074
+#define RESULT__RANGE_REFERENCE_AMB_COUNT 0x078
+#define RESULT__RANGE_RETURN_CONV_TIME 0x07C
+#define RESULT__RANGE_REFERENCE_CONV_TIME 0x080
+#define READOUT__AVERAGING_SAMPLE_PERIOD 0x10A
+#define FIRMWARE__BOOTUP 0x119
+#define FIRMWARE__RESULT_SCALER 0x120
+#define I2C_SLAVE__DEVICE_ADDRESS 0x212
+#define INTERLEAVED_MODE__ENABLE 0x243
+
 void VL6180x_setRegister(uint16_t registerAddr, uint8_t sendData, uint8_t byteSize);
 uint8_t VL6180x_getRegister(uint16_t registerAddr, uint8_t byteSize);
 
 //TODO
 
-uint8_t VL6180x_init(){
+uint8_t VL6180x_init(){x
    uint8_t data = VL6180x_getRegister(SYSTEM__FRESH_OUT_OF_RESET, ONE_BYTE);
 
-   if(data!=1){
+   if(data != 1){
        //reset failed
        return 0;
    }
@@ -46,7 +109,7 @@ uint8_t VL6180x_init(){
    VL6180x_setRegister(0x00bb, 0x3c, ONE_BYTE);
    VL6180x_setRegister(0x00b2, 0x09, ONE_BYTE);
    VL6180x_setRegister(0x00ca, 0x09, ONE_BYTE);
-   VL6180x_setRegister(0x0198, 0x01, ONE_BYTE);
+   VL6180x_setRegister(0x0198, 0x01, ONE_BYTE)
    VL6180x_setRegister(0x01b0, 0x17, ONE_BYTE);
    VL6180x_setRegister(0x01ad, 0x00, ONE_BYTE);
    VL6180x_setRegister(0x00ff, 0x05, ONE_BYTE);
@@ -72,6 +135,7 @@ uint8_t VL6180x_init(){
 }
 
 uint8_t VL6180x_getDistance(){
+    //measured in mm
     VL6180x_setRegister(SYSRANGE__START,0x02, ONE_BYTE); //select single shot mode
     while((VL6180x_getRegister(RESULT__INTERRUPT_STATUS_GPIO, ONE_BYTE) & 0x02)>>1 != 1){};
     uint8_t asl_data = VL6180x_getRegister(RESULT__RANGE_VAL, ONE_BYTE); // 1 byte
@@ -80,29 +144,9 @@ uint8_t VL6180x_getDistance(){
 }
 
 
-
-void VL6180x_initCommunication(){
-    //initialization for SDL and SCL pins used, can be modified
-    uint32_t systemClock;
-    systemClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN |
-            SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
-            120000000);
-
-    MAP_GPIOPinConfigure(GPIO_PG0_I2C1SCL);
-    MAP_GPIOPinConfigure(GPIO_PG1_I2C1SDA);
-    MAP_GPIOPinTypeI2C(GPIO_PORTG_BASE, GPIO_PIN_1);
-    MAP_GPIOPinTypeI2CSCL(GPIO_PORTG_BASE, GPIO_PIN_0);
-    GPIOG->PUR |= (GPIO_PIN_1 | GPIO_PIN_0);
-    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
-    while(!(MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_I2C1)))
-    {
-    }
-    MAP_I2CMasterInitExpClk(I2C1_BASE, systemClock, false);
-}
-
 /*private functions*/
 
-void VL6180x_setRegister(uint16_t registerAddr, uint16_t sendData, uint8_t byteSize){
+static void VL6180x_setRegister(uint16_t registerAddr, uint16_t sendData, uint8_t byteSize){
     //send 2 byte address
     MAP_I2CMasterSlaveAddrSet(I2C1_BASE, VL6180X_ADDRESS, false);
     MAP_I2CMasterDataPut(I2C1_BASE, registerAddr & 0xFF);
@@ -123,7 +167,7 @@ void VL6180x_setRegister(uint16_t registerAddr, uint16_t sendData, uint8_t byteS
 
 }
 
-uint8_t VL6180x_getRegister(uint16_t registerAddr, uint8_t byteSize){
+static uint8_t VL6180x_getRegister(uint16_t registerAddr, uint8_t byteSize){
     //send 2 byte address
     MAP_I2CMasterSlaveAddrSet(I2C1_BASE, VL6180X_ADDRESS, false);
     MAP_I2CMasterDataPut(I2C1_BASE, registerAddr & 0xFF);
@@ -136,7 +180,7 @@ uint8_t VL6180x_getRegister(uint16_t registerAddr, uint8_t byteSize){
         MAP_I2CMasterSlaveAddrSet(I2C1_BASE, VL6180X_ADDRESS, true);
         MAP_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
         receiveData = MAP_I2CMasterDataGet(I2C1_BASE);
-    }if else(byteSize == TWO_BYTES){
+    }else if(byteSize == TWO_BYTES){
         MAP_I2CMasterSlaveAddrSet(I2C1_BASE, VL6180X_ADDRESS, true);
         MAP_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
         uint8_t data_LSB = MAP_I2CMasterDataGet(I2C1_BASE);
